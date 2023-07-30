31899f7d280ac563ecc1042e55e7e53075550409
x2047 <138368953+x2047@users.noreply.github.com>
2023-07-30 20:55:24 +0900

    一時保存(6回目)

---
+++ script.js
@@ -0,0 +1,16 @@
+const popup = document.querySelectorAll(".popup");
+const popupback = document.getElementById("popupback");
+const popupimg = document.getElementById("popupimg")
+popup.forEach(function(value){
+    value.addEventListener("click",kakudai);
+});
+function kakudai(e){
+    popupback.style.display = "flex";
+    popupimg.setAttribute("src",e.target.getAttribute("src"));
+    popupimg.classList.add("transparent");
+}
+popupback.addEventListener("click",modosu);
+function modosu(){
+    popupback.style.display = "none";
+    popupimg.classList.remove("transparent");
+}
